5032df2d3168643b9f784d62c5199143d97c699c
Robbie Wagner <robbie.wagner@my.simpson.edu>
2023-03-31 20:31:14 -0500

    work on #1

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -1,3 +1,6 @@
+//References:
+//Use of Perlin Noise: https://www.youtube.com/watch?v=DBjd7NHMgOE
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +8,22 @@ using UnityEngine;
 public class TileGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject[] tileOptions;
-    [SerializeField] private float maxRows;
-    [SerializeField] private float maxColumns;
+    [SerializeField] private int maxRows;
+    [SerializeField] private int maxColumns;
     private float tileSize;
+    float[,] noiseMap;
     private Vector3 startingPosition;
+
+    private float PERLIN_SCALE = .1f;
 
     private void Start()
     {
         tileSize = tileOptions[0].transform.localScale.x;
-        startingPosition = new Vector3(-maxRows * tileSize / 2, -maxColumns * tileSize / 2, 0f);
+        float startingXPosition = (-maxRows * tileSize / 2) - (-maxRows * tileSize / 2) % tileSize;
+        float startingYPosition = (-maxColumns * tileSize / 2) - (-maxColumns * tileSize / 2) % tileSize;
+        startingPosition = new Vector3(startingXPosition, startingYPosition, 0f);
+
+        noiseMap = InitializeNoiseMap();
 
         GenerateTilemap();
     }
@@ -23,15 +33,17 @@ public class TileGenerator : MonoBehaviour
         int tile;
         Vector3 tilePosition;
 
-        for (float i = 0; i < maxRows; i++)
+        for (int y = 0; y < maxRows; y++)
         {
-            for (float j = 0; j < maxColumns; j++)
+            for (int x = 0; x < maxColumns; x++)
             {
-                tilePosition = new Vector3(tileSize * i, tileSize * j, 0) + startingPosition;
+                tilePosition = new Vector3(tileSize * y, tileSize * x, 0) + startingPosition;
                 if (PositionHasNoTile(tilePosition))
                 {
-                    //Debug.Log(i * 10 + j + 1);
-                    tile = Random.Range(0, tileOptions.Length);
+                    //Debug.Log(y * 10 + x + 1);
+                    tile = (int)noiseMap[x, y];
+                    if (tile < 0) tile = 0;
+                    else if (tile == tileOptions.Length) tile = tileOptions.Length - 1;
                     AddTile(tilePosition, tileOptions[tile]);
                 }
             }
@@ -55,5 +67,23 @@ public class TileGenerator : MonoBehaviour
             return false;
         }
         return true;
+    }
+
+    private float[,] InitializeNoiseMap()
+    {
+        float[,] initialNoiseMap = new float[maxColumns, maxRows];
+        float xOffset = Random.Range(-10000f, 10000f);
+        float yOffset = Random.Range(-10000f, 10000f);
+
+        for(int y = 0; y < maxRows; y++)
+        {
+            for(int x = 0; x < maxColumns; x++)
+            {
+                float noiseValue = Mathf.PerlinNoise(x * PERLIN_SCALE + xOffset, y * PERLIN_SCALE + yOffset);
+                initialNoiseMap[x,y] = noiseValue * tileOptions.Length;
+            }
+        }
+
+        return initialNoiseMap;
     }
 }
